92545ef5ec095e8cb44200f5039b76151a59475f
Sara <sara.rebecca360@gmail.com>
2025-03-24 18:01:01 +0100

    Solve exercises 08 - 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  let arrAge = arr.reduce((prev, curr) => {
+    if (
+      age(prev.yearOfBirth, prev.yearOfDeath) >
+      age(curr.yearOfBirth, curr.yearOfDeath)
+    ) {
+      return prev;
+    } else {
+      return curr;
+    }
+  });
+  return arrAge;
 };
+
+function age(birth, death = new Date().getFullYear()) {
+  return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
